debe43dcd2d62551a72d57d5fa9e2f8d9af9c053
Quahu <quahuu@gmail.com>
2021-11-01 00:54:14 +0100

    Updated the RentedArray structure.

---
+++ src/Qommon/Pooling/RentedArray.Static.cs
@@ -0,0 +1,58 @@
+﻿using System.Buffers;
+using System.Runtime.CompilerServices;
+
+namespace Qommon.Pooling
+{
+    public readonly partial struct RentedArray<T>
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> Rent(int length, bool clearArray = false)
+            => Rent(length, ArrayPool<T>.Shared, clearArray);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> Rent(int length, ArrayPool<T> pool, bool clearArray = false)
+        {
+            Guard.IsNotNull(pool);
+
+            return new(new(pool.Rent(length), 0, length), pool, clearArray);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> RentFor(T item)
+        {
+            var array = Rent(1);
+            array[0] = item;
+            return array;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> RentFor(T item1, T item2)
+        {
+            var array = Rent(2);
+            array[0] = item1;
+            array[1] = item2;
+            return array;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> RentFor(T item1, T item2, T item3)
+        {
+            var array = Rent(3);
+            array[0] = item1;
+            array[1] = item2;
+            array[2] = item3;
+            return array;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RentedArray<T> RentFor(T item1, T item2, T item3, T item4)
+        {
+            var array = Rent(4);
+            array[0] = item1;
+            array[1] = item2;
+            array[2] = item3;
+            array[3] = item4;
+            return array;
+        }
+    }
+}
--- src/Qommon/Pooling/RentedArray.cs
+++ src/Qommon/Pooling/RentedArray.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Buffers;
-using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
@@ -8,124 +7,31 @@ namespace Qommon.Pooling
 {
     /// <summary>
     ///     Represents an array rented from an <see cref="ArrayPool{T}"/> that will be returned to it on disposal.
-    ///     Does not perform any validation checks on the underlying array nor on the pool.
     /// </summary>
+    /// <remarks>
+    ///     Does not perform any validation checks on the underlying array nor on the pool.
+    /// </remarks>
     /// <typeparam name="T"> The type of the elements in the array. </typeparam>
-    public readonly struct RentedArray<T> : IList<T>, IReadOnlyList<T>, IDisposable
+    public readonly partial struct RentedArray<T> : IList<T>, IReadOnlyList<T>, IDisposable
     {
-        public int Length => _segment.Count;
-
-        public T this[int index]
-        {
-            get => _segment[index];
-            set => _segment[index] = value;
-        }
-
-        public RentedArray<T> this[Range range] => new(_segment[range], _pool);
-
         private readonly ArraySegment<T> _segment;
         private readonly ArrayPool<T> _pool;
+        private readonly bool _clearArray;
 
-        public RentedArray(ArraySegment<T> segment, ArrayPool<T> pool)
+        public RentedArray(ArraySegment<T> segment, ArrayPool<T> pool, bool clearArray)
         {
             Guard.IsNotNull(pool);
 
             _segment = segment;
             _pool = pool;
+            _clearArray = clearArray;
         }
-
-        public void CopyTo(T[] array)
-            => _segment.CopyTo(array);
-
-        public void CopyTo(Span<T> destination)
-            => _segment.AsSpan().CopyTo(destination);
-
-        public void CopyTo(T[] array, int arrayIndex)
-            => _segment.CopyTo(array, arrayIndex);
-
-        public ArraySegment<T>.Enumerator GetEnumerator()
-            => _segment.GetEnumerator();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Dispose()
         {
             if (_segment.Array != null)
-                _pool.Return(_segment.Array);
+                _pool.Return(_segment.Array, _clearArray);
         }
-
-        public ArraySegment<T> AsArraySegment()
-            => _segment;
-
-        public Memory<T> AsMemory()
-            => _segment;
-
-        public Memory<T> AsMemory(int start)
-            => _segment.AsMemory(start);
-
-        public Memory<T> AsMemory(int start, int length)
-            => _segment.AsMemory(start, length);
-
-        public Span<T> AsSpan()
-            => _segment;
-
-        public static implicit operator ArraySegment<T>(RentedArray<T> value)
-            => value._segment;
-
-        public static implicit operator Memory<T>(RentedArray<T> value)
-            => value._segment;
-
-        public static implicit operator ReadOnlyMemory<T>(RentedArray<T> value)
-            => value._segment;
-
-        public static implicit operator Span<T>(RentedArray<T> value)
-            => value._segment;
-
-        public static implicit operator ReadOnlySpan<T>(RentedArray<T> value)
-            => value._segment;
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RentedArray<T> Rent(int length)
-            => Rent(length, ArrayPool<T>.Shared);
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static RentedArray<T> Rent(int length, ArrayPool<T> pool)
-        {
-            Guard.IsNotNull(pool);
-
-            return new(new(pool.Rent(length), 0, length), pool);
-        }
-
-        int ICollection<T>.Count => _segment.Count;
-
-        int IReadOnlyCollection<T>.Count => _segment.Count;
-
-        bool ICollection<T>.IsReadOnly => false;
-
-        int IList<T>.IndexOf(T item)
-            => throw new NotSupportedException();
-
-        void IList<T>.Insert(int index, T item)
-            => throw new NotSupportedException();
-
-        void IList<T>.RemoveAt(int index)
-            => throw new NotSupportedException();
-
-        void ICollection<T>.Add(T item)
-            => throw new NotSupportedException();
-
-        void ICollection<T>.Clear()
-            => throw new NotSupportedException();
-
-        bool ICollection<T>.Contains(T item)
-            => throw new NotSupportedException();
-
-        bool ICollection<T>.Remove(T item)
-            => throw new NotSupportedException();
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-            => GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator()
-            => GetEnumerator();
     }
 }
