9967d4cf0d4b2e6c6712ee074a81c53086165b68
Quahu <quahuu@gmail.com>
2022-01-04 21:36:02 +0100

    Added a non-generic GetArray().

--- src/Qommon/Collections/EnumerableExtensions.cs
+++ src/Qommon/Collections/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -8,15 +9,8 @@ namespace Qommon.Collections
     [EditorBrowsable(EditorBrowsableState.Never)]
     public static class EnumerableExtensions
     {
-        /// <summary>
-        ///     Returns an array for this enumerable.
-        ///     If the source is <see langword="null"/> an empty array is returned.
-        ///     If the source is already an array it is returned directly.
-        ///     Otherwise the source is copied to a new array.
-        /// </summary>
-        /// <param name="source"> The enumerable to get </param>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
+        /// <inheritdoc cref="GetArray"/>
+        /// <typeparam name="T"> The type of the elements. </typeparam>
         public static T[] GetArray<T>(this IEnumerable<T> source)
         {
             return source switch
@@ -27,8 +21,28 @@ namespace Qommon.Collections
             };
         }
 
-        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source,
-            IEqualityComparer<TKey> comparer = null)
+        /// <summary>
+        ///     Returns an array for this enumerable.
+        ///     If the source is <see langword="null"/> an empty array is returned.
+        ///     If the source is already an array it is returned directly.
+        ///     Otherwise the source is copied to a new array.
+        /// </summary>
+        /// <param name="source"> The enumerable to get </param>
+        /// <returns>
+        ///     An array containing the elements from the enumerable.
+        /// </returns>
+        public static object[] GetArray(this IEnumerable source)
+        {
+            if (source is IEnumerable<object> genericSource)
+                return genericSource.GetArray();
+
+            if (source == null)
+                return Array.Empty<object>();
+
+            return source.Cast<object>().GetArray();
+        }
+
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source, IEqualityComparer<TKey> comparer = null)
         {
             return new(source, comparer);
         }
