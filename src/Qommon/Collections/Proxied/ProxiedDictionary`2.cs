9138d1941a7578da442f1a785da1a724cccb0d0b
Quahu <quahuu@gmail.com>
2022-01-06 09:05:26 +0100

    Updated the proxied collections.

--- src/Qommon/Collections/Proxied/ProxiedDictionary`2.cs
+++ src/Qommon/Collections/Proxied/ProxiedDictionary`2.cs
@@ -4,24 +4,19 @@ using System.Collections.Generic;
 namespace Qommon.Collections.Proxied
 {
     /// <summary>
-    ///     Represents an implementation of <see cref="IDictionary{TKey,TValue}"/> and <see cref="IReadOnlyDictionary{TKey, TValue}"/>
+    ///     Represents an implementation of <see cref="IDictionary{TKey,TValue}"/> and <see cref="IReadOnlyDictionary{TKey,TValue}"/>
     ///     by wrapping an <see cref="IDictionary{TKey, TValue}"/> with virtual implementations making overriding individual members very simple.
     /// </summary>
     /// <typeparam name="TKey"> The type of keys in the dictionary. </typeparam>
     /// <typeparam name="TValue"> The type of values in the dictionary. </typeparam>
-    public abstract class ProxiedDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>
+    public class ProxiedDictionary<TKey, TValue> : ProxiedCollection<KeyValuePair<TKey, TValue>>,
+        IDictionary<TKey, TValue>, IDictionary, IReadOnlyDictionary<TKey, TValue>
     {
         /// <inheritdoc/>
         public virtual ICollection<TKey> Keys => Dictionary.Keys;
 
         /// <inheritdoc/>
         public virtual ICollection<TValue> Values => Dictionary.Values;
-
-        /// <inheritdoc cref="ICollection{T}.Count"/>
-        public virtual int Count => Dictionary.Count;
-
-        /// <inheritdoc/>
-        public virtual bool IsReadOnly => Dictionary.IsReadOnly;
 
         /// <inheritdoc cref="IDictionary{TKey,TValue}.this"/>
         public virtual TValue this[TKey key]
@@ -37,7 +32,7 @@ namespace Qommon.Collections.Proxied
         /// <summary>
         ///     Gets the wrapped dictionary.
         /// </summary>
-        protected IDictionary<TKey, TValue> Dictionary { get; }
+        protected virtual IDictionary<TKey, TValue> Dictionary => Collection as IDictionary<TKey, TValue>;
 
         /// <summary>
         ///     Instantiates a new <see cref="ProxiedDictionary{TKey, TValue}"/> wrapping a <see cref="Dictionary{TKey,TValue}"/>.
@@ -45,36 +40,24 @@ namespace Qommon.Collections.Proxied
         /// <param name="capacity"> The initial capacity of the dictionary. </param>
         /// <param name="comparer"> The equality comparer for the dictionary keys. </param>
         protected ProxiedDictionary(int capacity = 0, IEqualityComparer<TKey> comparer = null)
-        {
-            Dictionary = new Dictionary<TKey, TValue>(capacity, comparer);
-        }
+            : base(new Dictionary<TKey, TValue>(capacity, comparer))
+        { }
 
         /// <summary>
         ///     Instantiates a new <see cref="ProxiedDictionary{TKey, TValue}"/>.
         /// </summary>
         /// <param name="dictionary"> The dictionary to wrap. </param>
-        protected ProxiedDictionary(IDictionary<TKey, TValue> dictionary)
-        {
-            Guard.IsNotNull(dictionary);
-
-            Dictionary = dictionary;
-        }
+        public ProxiedDictionary(IDictionary<TKey, TValue> dictionary)
+            : base(dictionary)
+        { }
 
         /// <inheritdoc/>
         public virtual void Add(TKey key, TValue value)
             => Dictionary.Add(key, value);
 
-        /// <inheritdoc/>
-        public virtual void Clear()
-            => Dictionary.Clear();
-
         /// <inheritdoc cref="IDictionary{TKey,TValue}.ContainsKey"/>
         public virtual bool ContainsKey(TKey key)
             => Dictionary.ContainsKey(key);
-
-        /// <inheritdoc/>
-        public virtual void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-            => Dictionary.CopyTo(array, arrayIndex);
 
         /// <inheritdoc/>
         public virtual bool Remove(TKey key)
@@ -84,20 +67,62 @@ namespace Qommon.Collections.Proxied
         public virtual bool TryGetValue(TKey key, out TValue value)
             => Dictionary.TryGetValue(key, out value);
 
-        /// <inheritdoc/>
-        public virtual IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-            => Dictionary.GetEnumerator();
+        bool IDictionary.IsFixedSize => (Dictionary as IDictionary)?.IsFixedSize ?? false;
 
-        IEnumerator IEnumerable.GetEnumerator()
-            => GetEnumerator();
+        ICollection IDictionary.Keys => Keys as ICollection;
 
-        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
-            => Add(item.Key, item.Value);
+        ICollection IDictionary.Values => Values as ICollection;
 
-        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
-            => ContainsKey(item.Key);
+        object IDictionary.this[object key]
+        {
+            get => this[(TKey) key];
+            set => this[(TKey) key] = (TValue) value;
+        }
 
-        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
-            => Remove(item.Key);
+        void IDictionary.Add(object key, object value)
+        {
+            Add((TKey) key, (TValue) value);
+        }
+
+        bool IDictionary.Contains(object key)
+        {
+            return ContainsKey((TKey) key);
+        }
+
+        void IDictionary.Remove(object key)
+        {
+            Remove((TKey) key);
+        }
+
+        IDictionaryEnumerator IDictionary.GetEnumerator()
+            => new Enumerator(GetEnumerator());
+
+        private sealed class Enumerator : IDictionaryEnumerator
+        {
+            public object Current => _enumerator.Current;
+
+            public DictionaryEntry Entry => new(_enumerator.Current.Key, _enumerator.Current.Value);
+
+            public object Key => _enumerator.Current.Key;
+
+            public object Value => _enumerator.Current.Value;
+
+            private readonly IEnumerator<KeyValuePair<TKey, TValue>> _enumerator;
+
+            public Enumerator(IEnumerator<KeyValuePair<TKey, TValue>> enumerator)
+            {
+                _enumerator = enumerator;
+            }
+
+            public bool MoveNext()
+            {
+                return _enumerator.MoveNext();
+            }
+
+            public void Reset()
+            {
+                _enumerator.Reset();
+            }
+        }
     }
 }
--- src/Qommon/Collections/Proxied/ProxiedList`1.cs
+++ src/Qommon/Collections/Proxied/ProxiedList`1.cs
@@ -8,14 +8,9 @@ namespace Qommon.Collections.Proxied
     ///     by wrapping an <see cref="IList{T}"/> with virtual implementations making overriding individual members very simple.
     /// </summary>
     /// <typeparam name="T"> The type of values in the list. </typeparam>
-    public abstract class ProxiedList<T> : IList<T>, IReadOnlyList<T>
+    public class ProxiedList<T> : ProxiedCollection<T>,
+        IList<T>, IList, IReadOnlyList<T>
     {
-        /// <inheritdoc cref="ICollection{T}.Count"/>
-        public virtual int Count => List.Count;
-
-        /// <inheritdoc/>
-        public virtual bool IsReadOnly => List.IsReadOnly;
-
         /// <inheritdoc cref="IList{T}.this"/>
         public virtual T this[int index]
         {
@@ -26,47 +21,23 @@ namespace Qommon.Collections.Proxied
         /// <summary>
         ///     Gets the wrapped list.
         /// </summary>
-        protected IList<T> List { get; }
+        protected virtual IList<T> List => Collection as IList<T>;
 
         /// <summary>
         ///     Instantiates a new <see cref="ProxiedList{T}"/> wrapping a <see cref="List{T}"/>.
         /// </summary>
         /// <param name="capacity"> The initial capacity of the list. </param>
         protected ProxiedList(int capacity = 0)
-        {
-            List = new List<T>(capacity);
-        }
+            : base(new List<T>(capacity))
+        { }
 
         /// <summary>
         ///     Instantiates a new <see cref="ProxiedList{T}"/>.
         /// </summary>
         /// <param name="list"> The list to wrap. </param>
-        protected ProxiedList(IList<T> list)
-        {
-            Guard.IsNotNull(list);
-
-            List = list;
-        }
-
-        /// <inheritdoc/>
-        public virtual void Add(T item)
-            => List.Add(item);
-
-        /// <inheritdoc/>
-        public virtual void Clear()
-            => List.Clear();
-
-        /// <inheritdoc/>
-        public virtual bool Contains(T item)
-            => List.Contains(item);
-
-        /// <inheritdoc/>
-        public virtual void CopyTo(T[] array, int arrayIndex)
-            => List.CopyTo(array, arrayIndex);
-
-        /// <inheritdoc/>
-        public virtual bool Remove(T item)
-            => List.Remove(item);
+        public ProxiedList(IList<T> list)
+            : base(list)
+        { }
 
         /// <inheritdoc/>
         public virtual int IndexOf(T item)
@@ -76,15 +47,42 @@ namespace Qommon.Collections.Proxied
         public virtual void Insert(int index, T item)
             => List.Insert(index, item);
 
-        /// <inheritdoc/>
+        /// <inheritdoc cref="IList{T}.RemoveAt"/>
         public virtual void RemoveAt(int index)
             => List.RemoveAt(index);
 
-        /// <inheritdoc/>
-        public virtual IEnumerator<T> GetEnumerator()
-            => List.GetEnumerator();
+        bool IList.IsFixedSize => (List as IList)?.IsFixedSize ?? false;
 
-        IEnumerator IEnumerable.GetEnumerator()
-            => GetEnumerator();
+        object IList.this[int index]
+        {
+            get => this[index];
+            set => this[index] = (T) value;
+        }
+
+        int IList.Add(object value)
+        {
+            Add((T) value);
+            return Count - 1; // Violates the IList contract, but in practice doesn't matter
+        }
+
+        bool IList.Contains(object value)
+        {
+            return Contains((T) value);
+        }
+
+        int IList.IndexOf(object value)
+        {
+            return IndexOf((T) value);
+        }
+
+        void IList.Insert(int index, object value)
+        {
+            Insert(index, (T) value);
+        }
+
+        void IList.Remove(object value)
+        {
+            Remove((T) value);
+        }
     }
 }
