01e75ff70845eb61deaddea72e28dfcc64ef7034
Quahu <quahuu@gmail.com>
2022-02-13 06:56:25 +0100

    Added the tests project.

---
+++ src/Qommon.Tests/QommonFixture.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using NUnit.Framework;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Serilog.Sinks.SystemConsole.Themes;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace Qommon.Tests
+{
+    [FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+    public abstract unsafe class QommonFixture
+    {
+        protected ILogger Logger;
+
+        protected TestContext _context;
+
+        protected static readonly ILoggerFactory LoggerFactory;
+
+        static QommonFixture()
+        {
+            var serilogLogger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+            LoggerFactory = new SerilogLoggerFactory(serilogLogger);
+        }
+
+        [SetUp]
+        public virtual void Setup()
+        {
+            _context = TestContext.CurrentContext;
+
+            Logger = LoggerFactory.CreateLogger(_context.Test.Name);
+        }
+
+        [TearDown]
+        public virtual void Teardown()
+        { }
+    }
+}
